19d092d9ceda9e07ac85644514cdcaf805d203aa
Anuraag <thehray@gmail.com>
2019-01-05 22:18:37 +0530

    eslint added

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "standard",
+    "rules":{
+        "no-var": "error",
+        "semi": ["error", "always"],
+        "no-else-return": 2,
+        "no-shadow": 2,
+        "no-unused-vars": 2,
+        "arrow-spacing": 2
+    }
+};
