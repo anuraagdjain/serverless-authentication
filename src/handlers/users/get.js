a493361257c308710f386956fce632beabed1ecb
Anuraag <thehray@gmail.com>
2019-01-05 21:25:26 +0530

    get user info api

---
+++ src/handlers/users/get.js
@@ -0,0 +1,10 @@
+'use strict';
+const response = require("../../utils/response");
+module.exports.handler = async  (event,context) => {
+    let { user } = event.requestContext.authorizer;
+    user = JSON.parse(user);
+    if (user._id.toString() !== event.pathParameters.id) {
+        return response.serverError(401, { message: 'Unauthorized' });
+    }
+    return response.success({ user });
+}
